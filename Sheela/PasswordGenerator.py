af3b58340098c74f665f39abfb8e9cf5edf04c68
sheela <shesiva@gmail.com>
2021-01-20 16:20:33 -0500

    Sheela Commit _ Password Generator program

---
+++ Sheela/PasswordGenerator.py
@@ -0,0 +1,44 @@
+import random
+import string
+
+class PasswordGen :
+    def __init__(self):
+        self.passlist = []
+
+    def autopasswordgen(self,numpass,passlength):
+
+        digit1 = random.choice(string.ascii_uppercase)
+        digit2 = random.choice(string.ascii_lowercase)
+        digit3 = random.choice(string.digits)
+        digit4 = random.choice(string.punctuation)
+        chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
+        for p in range(numpass):
+            password = ""
+            for a in range(4):
+                password = digit1 + digit2 + digit3 + digit4
+            for b in range(passlength - 4):
+                password = password + random.choice(chars)
+            self.passlist.append(password)
+        return self.passlist
+
+
+
+if __name__ == '__main__':
+    print("AUTOMATIC PASSWORD GENERATOR")
+    while True:
+        number = int(input("Enter Number of Passwords to be generated :"))
+        if number == 0:
+            print("Its cant be 0. Enter at least 1")
+        else:
+            while True:
+                length = int(input("Enter the length(min8) of the password to be generated"))
+                if length < 8:
+                    print("Its cant be less than 8. Enter a number 8 or more than 8")
+                else:
+                    passwordgen = PasswordGen()
+                    passwordlist = passwordgen.autopasswordgen(number, length)
+                    print(str(number) + "  Passwords Generated :")
+                    for i in range(len(passwordlist)):
+                        print(passwordlist[i])
+                    break
+            break
