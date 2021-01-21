6c6decd47d4358f4a80599c632a37f6a9f5ec2d0
Biji <bijieliza@gmail.com>
2021-01-20 20:10:57 -0500

    Biji commit01/20/2021a

---
+++ Biji/Passwordgenerator.py
@@ -0,0 +1,71 @@
+import random
+import string
+import json
+
+
+class Passwordgenerator:
+
+    def createrandompwdletters(self):
+        passwordlength = int(input("Password Length:"))
+        passwordcharacters = string.ascii_letters
+        password = []
+        for x in range(passwordlength):
+            (password.append(random.choice(passwordcharacters)))
+        password = "".join(password)
+        print(password)
+
+    def createrandompwddigits(self):
+        passwordlength = int(input("Password Length:"))
+        passwordcharacters = string.digits
+        password = []
+        for x in range(passwordlength):
+            (password.append(random.choice(passwordcharacters)))
+        password = "".join(password)
+        print(password)
+
+    def createrandompwdothercharacters(self):
+        passwordlength = int(input("Password Length:"))
+        passwordcharacters =string.punctuation
+        password = []
+        for x in range(passwordlength):
+            (password.append(random.choice(passwordcharacters)))
+        password = "".join(password)
+        print(password)
+
+    def createrandompwdexcludecharacters(self):
+        passwordlength = int(input("Password Length:"))
+        passwordcharacters = string.punctuation + string.digits
+        password = []
+        for x in range(passwordlength):
+            (password.append(random.choice(passwordcharacters)))
+        password = "".join(password)
+        print(password)
+
+if __name__ == "__main__":
+    print(" Password Generator")
+
+option_dict = {
+    1: 'Letters ',
+    2: 'Digits',
+    3: 'Othercharacters',
+    4: 'Exclude characters',
+    5: 'Exit'
+}
+print(json.dumps(option_dict, indent=4))
+
+objpwd = Passwordgenerator()
+
+while True:
+    choice = int(input("Select an option : "))
+    if choice == 1:
+        objpwd.createrandompwdletters()
+    elif choice == 2:
+        objpwd.createrandompwddigits()
+    elif choice == 3:
+        objpwd.createrandompwdothercharacters()
+    elif choice == 4:
+        objpwd.createrandompwdexcludecharacters()
+    elif choice == 5:
+        break
+    else:
+        print("Enter valid option ")
---
+++ Biji/Sendemail.py
@@ -0,0 +1,40 @@
+import smtplib
+from email.message import EmailMessage
+
+
+class Sendmail:
+
+    def __init__(self):
+        sender = ""
+        pwd = ""
+        receiver = ""
+        message=""
+
+    def getinputs(self):
+       self.sender = input("Enter your Email ID")
+       self. pwd = input("Enter your password")
+       self. receiver = input("TO: ")
+       self.  message=EmailMessage()
+       self.message['subject'] = input("Subject")
+       self.message['From'] =self.sender
+       self.message['To'] =self.receiver
+       self.message.set_content(input("Enter the message:"))
+
+    def sendmail(self):
+        try:
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.ehlo()
+            s.starttls()
+            s.login(self.sender,self.pwd)
+            s.sendmail(self.sender, self.receiver, self.message.as_string())
+            s.close()
+            print("Successfully sent email to '{}'".format(self.receiver))
+        except smtplib.SMTPException:
+            print("Error: unable to send email")
+
+
+if __name__ == '__main__':
+    objsendmail = Sendmail()
+    objsendmail.getinputs()
+    objsendmail.sendmail()
+
