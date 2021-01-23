014bc29679c210c7eca57035f1884b122878f365
sharedrepo2021 <susmita.chandra@zoho.com>
2021-01-22 19:41:36 -0500

    Adhoc

--- Susmita/DAY 09/Covid19.py
+++ Susmita/DAY 09/Covid19.py
@@ -9,17 +9,20 @@ country = input("Enter the country name:: ")
 df_temp = df1[df1['Country'] == country]
 days = int(input("How many days you want see:: "))
 print(df_temp.tail(days))
+print("Press 1 for Confirmed and Death graph")
+print("Press 2 for Confirmed and Recoverd graph")
+i = int(input("Enter Options:: "))
 #### ----- Step 3 (Plot data)----
 # Increase size of plot
-plt.rcParams["figure.figsize"] = 2, 2  # Remove if not on Jupyter
+plt.rcParams["figure.figsize"] = 3, 3  # Remove if not on Jupyter
 # Plot column 'Confirmed'
-
-df_temp.plot(kind='bar', x='Date', y='Confirmed', color='blue')
-ax1 = plt.gca()
-df_temp.plot(kind='bar', x='Date', y='Deaths', color='red', ax=ax1)
-plt.show()
-
-df_temp.plot(kind='bar', x='Date', y='Recovered', color='green')
-ax1 = plt.gca()
-df_temp.plot(kind='bar', x='Date', y='Confirmed', color='red', ax=ax1)
-plt.show()
+if i == 1:
+    df_temp.plot(kind='bar', x='Date', y='Confirmed', color='blue')
+    ax1 = plt.gca()
+    df_temp.plot(kind='bar', x='Date', y='Deaths', color='red', ax=ax1)
+    plt.show()
+else:
+    df_temp.plot(kind='bar', x='Date', y='Confirmed', color='green')
+    ax1 = plt.gca()
+    df_temp.plot(kind='bar', x='Date', y='Recovered', color='red', ax=ax1)
+    plt.show()
