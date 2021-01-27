cbd80d4047bbd2b30d704373fce4bdbdfeada71c
sharedrepo2021 <susmita.chandra@zoho.com>
2021-01-27 01:09:18 -0500

    Adhoc

---
+++ Susmita/DAY 09/Stock_market_live.py
@@ -0,0 +1,39 @@
+import pyodbc
+import pandas as pd
+
+
+class DBase:
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+        self.table_name = None
+
+    def connect(self):
+        self.conn = pyodbc.connect('Driver={SQL Server};'
+                                   'Server=DESKTOP-5H4SDFK\SQLEXPRESS;'
+                                   'Database=LocalDB;'
+                                   'Trusted_Connection=yes;')
+        self.cursor = self.conn.cursor()
+
+    def commit(self):
+        _query = 'COMMIT'
+        self.cursor.execute(_query)
+
+    def disconnect(self):
+        self.cursor.close()
+
+    def execute_sql_and_commit(self, sql):
+        try:
+            self.cursor.execute(sql)
+        except Exception as e:
+            print('Error in executing...Query: {}...Error: {}'.format(sql, e))
+        else:
+            self.commit()
+
+    def execute_sql(self, sql):
+        try:
+            sql_query_df = pd.read_sql_query(sql, self.conn)
+        except Exception as e:
+            print('Unable to execute query: {}...Error: {}'.format(sql, e))
+        else:
+            return sql_query_df
