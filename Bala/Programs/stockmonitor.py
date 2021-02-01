a041df9c70e66e16fd9d1c597f85b2375ef833a3
bala0 <bala08mur@gmail.com>
2021-02-01 00:01:44 -0500

    Commit_01 - 01/29/2021

--- Bala/Programs/stockmonitor.py
+++ Bala/Programs/stockmonitor.py
@@ -1,9 +1,10 @@
 import json
 import pyodbc
+import time
 import pandas as pd
 import datetime as dt
 
-
+from tabulate import tabulate
 from yahoo_fin import stock_info as si
 from urllib.request import urlopen
 from urllib.request import Request
@@ -26,56 +27,91 @@ class LoadSymbolDataFrame:
 
 class HandleDataBaseOperations:
     def __init__(self):
-        self.db_connection = None
-        self.db_cursor = None
-
-        self.db_db = 'StockMonitor'
-        self.db_schema = 'Stock'
+        pass
 
     def establish_db_connection(self):
+        global db_connection, db_cursor
+
         try:
-            self.db_connection = pyodbc.connect('Driver={SQL Server};'
+            db_connection = pyodbc.connect('Driver={SQL Server};'
                                        'Server=BALALENR7\SQLEXPRESS;'
                                        'Database=StockMonitor;'
                                        'Trusted_Connection=yes;')
 
-            self.db_cursor = self.db_connection.cursor()
+            db_cursor = db_connection.cursor()
         except Exception:
             print('Database Connection Failed !!!')
 
     def commit_database(self):
         try:
-            self.db_cursor.execute('COMMIT')
+            db_cursor.execute('COMMIT')
         except Exception:
             print('COMMIT Failed !!!')
 
-    def delete_stk_sym_table(self):
+class HandleStockSymbolTable:
+    def __init__(self):
+        global db_name, db_schema
+
+    def delete_stk_sym(self):
         try:
-            _deleteqry = 'DELETE FROM {}.{}.STK_SYMBOL_DETAILS;'.format(self.db_db, self.db_schema)
-            self.db_cursor.execute(_deleteqry)
+            _deleteqry = 'DELETE FROM {}.{}.STK_SYMBOL_DETAILS;'.format(db_name, db_schema)
+            db_cursor.execute(_deleteqry)
         except:
             print('SQL Error while deleting all rows from STK_SYMBOL_DETAILS table !!!')
 
-    def insert_stk_sym_table(self, insertdf):
+    def select_stk_sym(self, stk_sym_id):
+        try:
+            _selectqry = "SELECT * FROM {}.{}.STK_SYMBOL_DETAILS " \
+                         "WHERE Stk_Sym_Symbol='{}';".format(db_name, db_schema, stk_sym_id)
+            _sql1 = pd.read_sql_query(_selectqry, db_connection)
+            return(_sql1.shape[0])
+        except:
+            print('error Enter Valid Stock-Symbol-ID !!!')
+
+    def insert_stk_sym(self, insertdf):
         _curr_date_time = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         _insert_df = insertdf
         try:
             for index, rows in _insert_df.iterrows():
                 _rows_str = rows.Stock_Name.replace("'", "")
                 _insertqry = "INSERT INTO {}.{}.STK_SYMBOL_DETAILS " \
-                            "VALUES ({}, '{}', '{}', '{}')".format(self.db_db, self.db_schema, index,
-                                                                rows.Ticker_Symbol, _rows_str, _curr_date_time)
-                self.db_cursor.execute(_insertqry)
+                            "VALUES ({}, '{}', '{}', '{}')".format(db_name, db_schema, index,
+                                                                   rows.Ticker_Symbol, _rows_str, _curr_date_time)
+                db_cursor.execute(_insertqry)
         except:
             print('SQL Error while inserting rows from STK_SYMBOL_DETAILS table !!!')
+
+class HandleFavUsrStkSymTable:
+    def __init__(self):
+        global db_name, db_schema, db_user_id
+
+    def select_fav_usr_stk_sym(self):
+        try:
+            _selectqry = 'SELECT Fav_Usr_Stk_Sym_ID as Symbol_ID, Fav_Usr_Stk_Symbol as Stock_Symbol, ' \
+                         'Fav_Usr_Stk_Name as Stock_Name FROM {}.{}.FAV_USER_STK_SYMBOL_DETAILS ' \
+                         'WHERE Fav_Usr_Stk_User_ID={} ;'.format(db_name, db_schema, db_user_id)
+            _sql1 = pd.read_sql_query(_selectqry, db_connection)
+            if (_sql1.shape[0] > 0):
+                print(tabulate(_sql1, headers='keys', tablefmt="psql"))
+            else:
+                print('No Rows Selected !!!')
+        except:
+            print('Enter Valid User-ID !!!')
 
 
 if __name__ == '__main__':
 
+    db_connection = None
+    db_cursor = None
+    db_name = 'StockMonitor'
+    db_schema = 'Stock'
+    db_user_id = ''
+
     option_dict = {
-        0: 'Exit from the Application',
-        1: 'Refresh the Stock Symbols',
-        2: 'List User Favourite Stocks'
+        0: "Exit from the Application",
+        1: "Refresh the Stock Symbols",
+        2: "List User Favourite's Stocks",
+        3: "Change User Favourite's Stocks"
     }
 
     # Establish the Connections with Database
@@ -89,21 +125,62 @@ if __name__ == '__main__':
 
         if option == '0':
             break
+
         elif option == '1':
             # These statements below loads the latest stock in to a STK_SYMBOL_DETAILS
             load_symbol = LoadSymbolDataFrame()
             load_symbol_df = load_symbol.load_sym_table()
-            handle_db.delete_stk_sym_table()
-            handle_db.insert_stk_sym_table(load_symbol_df)
+
+            stock_symbol = HandleStockSymbolTable()
+            stock_symbol.delete_stk_sym()
+
+            str_print = 'STK_SYMBOL_DETAILS Refresh In-Progress ...'
+            for k in range(50):
+                print('\r' + str_print[:k], end='')
+                time.sleep(.05)
+            print('\r', end='')
+            stock_symbol.insert_stk_sym(load_symbol_df)
             handle_db.commit_database()
-            print('STK_SYMBOL_DETAILS Loaded Successfully.')
+            str_print = 'STK_SYMBOL_DETAILS Loaded Successfully!'
+            for k in range(50):
+                print('\r' + str_print[:k], end='')
+                time.sleep(.05)
+            print('\n')
+            stock_symbol = None
+
         elif option == '2':
-            # These statements below handles User's Favourite Stocks
-            load_symbol = LoadSymbolDataFrame()
-            load_symbol_df = load_symbol.load_sym_table()
-            handle_db.delete_stk_sym_table()
-            handle_db.insert_stk_sym_table(load_symbol_df)
-            handle_db.commit_database()
-            print('STK_SYMBOL_DETAILS Loaded Successfully.')
+            # These statements below list User Favourite's Stocks
+            db_user_id = input('Enter User ID: ')
+            if db_user_id.isnumeric():
+                db_user_id = int(db_user_id)
+                fav_usr_stk_sym = HandleFavUsrStkSymTable()
+                fav_usr_stk_sym.select_fav_usr_stk_sym()
+            else:
+                print('Enter Valid User-ID !!!')
+
+        elif option == '3':
+            #These statements below updates User Favourite's Stocks
+            db_user_id = input('Enter User ID: ')
+            if db_user_id.isnumeric():
+                db_user_id = int(db_user_id)
+            chg_fav = input("Do You want to Add your Favourite's (Y/N): ").upper()
+            if chg_fav == 'Y':
+                fav_stk_sym = input('Enter Favourite Stock Symbol: ').upper()
+                stock_symbol = HandleStockSymbolTable()
+                if (stock_symbol.select_stk_sym(fav_stk_sym) > 0):
+                    print('Stock Present')
+                else:
+                    print('Stock Not Present')
+
+            chg_fav = input("Do You want to Delete your Favourite's (Y/N): ").upper()
+            if chg_fav == 'Y':
+                fav_stk_sym = input('Enter Favourite Stock Symbol: ').upper()
+                stock_symbol = HandleStockSymbolTable()
+                if (stock_symbol.select_stk_sym(fav_stk_sym) > 0):
+                    print('Stock Present')
+                else:
+                    print('Stock Not Present')
+
         else:
             print('An Invalid Option. Try Valid Option !!!')
+
